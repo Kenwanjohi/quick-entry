b207a3e1713e61b7cff1747d0ce6fcde52b67cce
Kenwanjohi <wanjohiken85@gmail.com>
2024-03-02 12:48:11 +0300

    Add sessions route - login

---
+++ routes/sessions/routes.js
@@ -0,0 +1,52 @@
+import fp from "fastify-plugin";
+import bcrypt from "bcrypt";
+
+export default fp(
+  async function sessions(fastify, opts) {
+    fastify.post(
+      "/login",
+      {
+        schema: {
+          body: {
+            title: "User Login",
+            description: "User login details",
+            type: "object",
+            properties: {
+              username: { description: "username", type: "string" },
+              password: { description: "password", type: "string" },
+            },
+            required: ["username", "password"],
+            additionalProperties: false,
+          },
+        },
+      },
+      async function loginHandler(request, reply) {
+        try {
+          const { username, password } = request.body;
+          const user = await this.usersDataSource.getUser(username);
+          if (!user) {
+            reply.code(401);
+            return { authenticated: false, message: "Invalid credentials" };
+          }
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (!passwordMatch) {
+            reply.code(401);
+            return { authenticated: false, message: "Invalid credentials" };
+          }
+          request.user = { userId: user._id, username };
+          const accessToken = await request.createToken({ expiresIn: "30m" });
+
+          reply.code(200);
+          return { accessToken };
+        } catch (error) {
+          reply.code(500);
+          return { authenticated: false, message: "Internal server error" };
+        }
+      }
+    );
+  },
+  {
+    encapsulate: true,
+  }
+);
