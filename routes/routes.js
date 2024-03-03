0b18d481bf6b6a08d2e8482cdca7208fe427a1e4
Kenwanjohi <wanjohiken85@gmail.com>
2024-03-03 06:00:21 +0300

    Rename route

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+import fp from 'fastify-plugin'
+export default fp(
+  async function (fastify, opts) {
+    fastify.get('/', async function (request, reply) {
+      return { root: true }
+    })
+  },
+  { encapsulate: true }
+)
