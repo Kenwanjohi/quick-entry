7e7925f4c326e84d860877138dde96b6c0e06fac
Kenwanjohi <wanjohiken85@gmail.com>
2024-03-03 06:02:54 +0300

    Configure tests helper

--- test/helper.js
+++ test/helper.js
@@ -4,6 +4,7 @@
 import helper from 'fastify-cli/helper.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import { randomBytes } from 'crypto'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -11,19 +12,19 @@ const AppPath = path.join(__dirname, '..', 'app.js')
 
 // Fill in this config with all the configurations
 // needed for testing the application
-function config () {
-  return {}
+function config (env) {
+  return { ...env }
 }
 
 // automatically build and tear down our instance
-async function build (t) {
+async function build (t, env) {
   // you can set all the options supported by the fastify CLI command
   const argv = [AppPath]
 
   // fastify-plugin ensures that all decorators
   // are exposed for testing purposes, this is
   // different from the production setup
-  const app = await helper.build(argv, config())
+  const app = await helper.build(argv, config({ ...env }))
 
   // tear down our app after we are done
   t.after(() => app.close())
@@ -31,7 +32,22 @@ async function build (t) {
   return app
 }
 
-export {
-  config,
-  build
+async function buildUser (app) {
+  const username = randomBytes(16).toString('hex')
+  const password = '12345678'
+  const res = await app.inject({
+    method: 'POST',
+    url: 'accounts/register',
+    payload: {
+      username,
+      password
+    }
+  })
+  return {
+    username,
+    password,
+    accessToken: res.json().accessToken
+  }
 }
+
+export { config, build, buildUser }
